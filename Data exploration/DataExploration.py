daa923268db27b1a77e8de678e08984dd329d172
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-02-02 14:31:06 +0100

    Create DataExploration.py
    Data summary class

---
+++ Data exploration/DataExploration.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+
+class DataExploration:
+
+    def __init__(self, data_object):
+        self.data = data_object
+
+    def get_overview_of_data(self):
+        print('Dataframe info')
+        print(self.data.info())
+        print('Summary dataframe')
+        print(self.data.describe())
+
+    def get_histogram(self, variable_of_interest):
+        data_histogram = self.data.loc[:,variable_of_interest]
+        data_histogram_count = data_histogram.value_counts()
+        sns.displot(data_histogram)
+        return data_histogram_count
