28dfc8f35c21a5165b34b00e6fee3925cf1e07f3
Tim Kuijpers <tjm.kuijpers@gmail.com>
2022-02-04 09:54:23 +0100

    Added violin and lmplot

--- Data exploration/DataExploration.py
+++ Data exploration/DataExploration.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 class DataExploration:
 
@@ -15,8 +15,18 @@ class DataExploration:
     def get_histogram(self, variable_of_interest):
         data_histogram = self.data.loc[:,variable_of_interest]
         data_histogram_count = data_histogram.value_counts()
+        plt.figure('Histogram')
         sns.displot(data_histogram)
         return data_histogram_count
 
+    def create_lmplot(self,variables_of_interest,variable_of_interest_x,variable_of_interest_y):
+        plt.figure('lmplot')
+        data_lmplot = self.data.loc[:,variables_of_interest]
+        print(data_lmplot)
+        sns.lmplot(variable_of_interest_x,variable_of_interest_y,data=data_lmplot,fit_reg=False)
 
-    
+
+    def create_violin_plot(self,variable_of_interest):
+        plt.figure('Violin plot')
+        data_violin=self.data.loc[:,variable_of_interest]
+        sns.violinplot(data=data_violin)
