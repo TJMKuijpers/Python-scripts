7e5f1a799a475d31c028ff900f4329f561061230
Tim Kuijpers <tjm.kuijpers@gmail.com>
2022-02-09 17:18:42 +0100

    Added the option to set the regression line in lmplot

--- Data exploration/DataExploration.py
+++ Data exploration/DataExploration.py
@@ -19,11 +19,11 @@ class DataExploration:
         sns.displot(data_histogram)
         return data_histogram_count
 
-    def create_lmplot(self,variables_of_interest,variable_of_interest_x,variable_of_interest_y):
+    def create_lmplot(self,variables_of_interest,variable_of_interest_x,variable_of_interest_y,regression_line=False):
         plt.figure('lmplot')
         data_lmplot = self.data.loc[:,variables_of_interest]
         print(data_lmplot)
-        sns.lmplot(variable_of_interest_x,variable_of_interest_y,data=data_lmplot,fit_reg=False)
+        sns.lmplot(variable_of_interest_x,variable_of_interest_y,data=data_lmplot,fit_reg=regression_line)
 
 
     def create_violin_plot(self,variable_of_interest):
